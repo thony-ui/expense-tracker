10e14e60fc67f91a2c6592bdbac62fbc2ff331c0
thony-ui <anthonyhermanto1@gmail.com>
2025-06-18 21:20:43 +0800

    essential model fixes

--- frontend/app/_components/dashboard/modals/confirm-generate-report-modal.tsx
+++ frontend/app/_components/dashboard/modals/confirm-generate-report-modal.tsx
@@ -1,5 +1,4 @@
-import { useDeleteTransaction } from "@/app/mutations/use-delete-transaction";
-import { invalidateTransactions } from "@/app/queries/use-get-transactions";
+import { usePostGenerateExpenseReport } from "@/app/mutations/use-post-generate-expense";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
@@ -9,47 +8,46 @@ import {
   DialogTitle,
 } from "@/components/ui/dialog";
 import React from "react";
-import { toast } from "react-toastify";
 
-function DeleteTransactionModal({
+function ConfirmGenerateReportModal({
   open,
   setOpen,
-  transactionId,
+  setIsGenerating,
 }: {
   open: boolean;
   setOpen: React.Dispatch<React.SetStateAction<boolean>>;
-  transactionId: string;
+  setIsGenerating: React.Dispatch<React.SetStateAction<boolean>>;
 }) {
-  const { mutateAsync: deleteTransaction } = useDeleteTransaction();
-  const handleDelete = async (transactionId: string) => {
+  const { mutateAsync: postGenerateReport } =
+    usePostGenerateExpenseReport(setIsGenerating);
+  const handleConfirm = async () => {
     setOpen(false);
-    await deleteTransaction(transactionId);
-    toast("Transaction deleted successfully!", { type: "success" });
-    invalidateTransactions();
+    await postGenerateReport();
   };
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
-          <DialogTitle>Delete Transaction</DialogTitle>
+          <DialogTitle>Generate Report</DialogTitle>
           <p className="text-sm text-gray-500">
-            Are you sure you want to delete this transaction? This action cannot
-            be undone.
+            Are you sure you want to generate a report? This will create a new
+            report based on your current transactions.
           </p>
         </DialogHeader>
         <DialogFooter className="flex gap-2 flex-wrap flex-row justify-end sm:gap-0">
           <Button
             variant="secondary"
-            className="border-none"
+            className="border-none w-[100px]"
             onClick={() => setOpen(false)}
           >
             Cancel
           </Button>
           <Button
-            variant="destructive"
-            onClick={() => handleDelete(transactionId)}
+            variant="default"
+            onClick={handleConfirm}
+            className="w-[100px]"
           >
-            Delete
+            Confirm
           </Button>
         </DialogFooter>
       </DialogContent>
@@ -57,4 +55,4 @@ function DeleteTransactionModal({
   );
 }
 
-export default DeleteTransactionModal;
+export default ConfirmGenerateReportModal;
--- frontend/app/_components/layout/header.tsx
+++ frontend/app/_components/layout/header.tsx
@@ -1,13 +1,13 @@
 "use client";
 
-import { Bars3Icon, BellIcon } from "@heroicons/react/24/outline";
+import { Bars3Icon } from "@heroicons/react/24/outline";
 import { AddTransactionDialog } from "../forms/add-transaction-dialog";
 import { UserMenu } from "./user-menu";
 import { Button } from "@/components/ui/button";
 import { DownloadIcon } from "lucide-react";
-import { usePostGenerateExpenseReport } from "@/app/mutations/use-post-generate-expense";
 import { useState } from "react";
 import { GeneratingExpenseReportDialog } from "../generating-expense-report-modal";
+import ConfirmGenerateReportModal from "../dashboard/modals/confirm-generate-report-modal";
 
 interface HeaderProps {
   onMenuClick: () => void;
@@ -15,25 +15,31 @@ interface HeaderProps {
 
 export function Header({ onMenuClick }: HeaderProps) {
   const [isGenerating, setIsGenerating] = useState<boolean>(false);
-  const { mutateAsync: postGenerateReport } =
-    usePostGenerateExpenseReport(setIsGenerating);
+  const [isOpenConfirmGenerationModal, setIsOpenConfirmGenerationModal] =
+    useState<boolean>(false);
+
   return (
-    <div className="sticky top-0 z-40 flex h-16 shrink-0 items-center border-b border-gray-200 bg-white shadow-sm px-6">
+    <div className="sticky top-0 z-40 flex h-16 shrink-0 items-center border-b border-gray-200 bg-white shadow-sm pr-6 pl-4">
       <GeneratingExpenseReportDialog isGenerating={isGenerating} />
-      <button
-        type="button"
-        className="-m-2.5 p-2.5 text-gray-700 lg:hidden"
-        onClick={onMenuClick}
-      >
-        <Bars3Icon className="h-6 w-6" />
-      </button>
+      <ConfirmGenerateReportModal
+        open={isOpenConfirmGenerationModal}
+        setOpen={setIsOpenConfirmGenerationModal}
+        setIsGenerating={setIsGenerating}
+      />
 
       <div className="flex items-center justify-between w-full ml-2 lg:ml-0">
-        <div className="flex gap-2 justify-start">
+        <button
+          type="button"
+          className="text-gray-700 lg:hidden"
+          onClick={onMenuClick}
+        >
+          <Bars3Icon className="h-6 w-6" />
+        </button>
+        <div className="flex gap-2 ">
           <AddTransactionDialog />
           <Button
             className="flex items-center gap-2 bg-white text-black hover:bg-gray-100 hover:text-black border border-zinc-100"
-            onClick={async () => await postGenerateReport()}
+            onClick={() => setIsOpenConfirmGenerationModal(true)}
           >
             <DownloadIcon className="h-4 w-4" />
             <p className="hidden sm:block">Generate Report</p>
