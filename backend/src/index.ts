4d429053eb5cf332dc0c5f2a6f6fcf8555463ced
thony-ui <anthonyhermanto1@gmail.com>
2026-01-18 16:09:24 +0800

    Add health check

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -25,10 +25,14 @@ app.use(
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
-  })
+  }),
 );
 
 app.use(bodyParser.json());
+
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
 
 defineUserRoutes(app);
 defineTransactionRoutes(app);
